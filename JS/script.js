0c79680d98a4d5c4145467d1d87517a65f26b041
yurign000 <yurign000@gmail.com>
2023-04-26 10:40:22 -0300

    feat: add all project

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+function changePageOsi(page_select){
+    window.location.href = `page-osi${page_select}.html`
+}
+function changePageTcp(page_select){
+    window.location.href = `page-tcp${page_select}.html`
+}
